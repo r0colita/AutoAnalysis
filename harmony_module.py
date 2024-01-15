c68079948c433b76d086e740d039f0fa7038df8f
rocolita <bernie@dorg.tech>
2024-01-15 13:36:44 -0400

    removed the use of midi_to_note_translator dfrom the generate_roman_numerals function

--- harmony_module.py
+++ harmony_module.py
@@ -1,31 +1,30 @@
-from music21 import chord, note
-from mido import MidiFile, Message
-from midi_to_notation_translator import *
+import pandas as pd
+from music21 import *
+from mido import Message
+from midi_to_notation_translator import get_key_sigs
 
-mid = MidiFile('./test midi files/czerny_299_1.midi')
-key_sig = get_key_sig(mid)
+razumovsky = corpus.parse('beethoven/opus59no1/movement1')
+# Define file to be searched
+czerny = converter.parse('./test midi files/czerny_299_1.midi')
+
 
 
 # find the chords present in the midi file
-def find_chords(file):
-    chords = []
-    current_chord_notes =[]
-    current_chord_start_time = None
-    for msg in file.tracks[1]:
-        if not msg.is_meta and hasattr(msg, 'note'):
-            midi_note = msg.note
-            midi_to_note_name(midi_note)
-            note_name = midi_to_note_name(midi_note)
-            if msg.time > 0:
-                if current_chord_notes:
-                    chord_obj = chord.Chord(current_chord_notes)
-                    chords.append(chord_obj)
-                current_chord_start_time = msg.time
-                current_chord_notes = [note_name]
-            elif msg.time == 0:
-                current_chord_notes.append(note_name)
+def generate_roman_numerals(part):
+    keys = get_key_sigs(part)
+    chords = part.chordify(removeRedundantPitches=True)
+    numeral_list = []
 
-    #print(current_chord_notes)
-    print(chords)
+    for measure in chords.getElementsByClass(stream.Measure):
+        measure_chords = []
+        
+        for c in measure.flatten().getElementsByClass(chord.Chord):
+            rn = roman.romanNumeralFromChord(c, keys)
+            measure_chords.append(rn.figure)
+    
+        numeral_list.append(measure_chords)
+    return numeral_list
 
-find_chords(mid)
+
+#help(key.Key)
+print(generate_roman_numerals(razumovsky))
