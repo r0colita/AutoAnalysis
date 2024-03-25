b0927292ea4cb88fd7f778230aa3e515140f35dc
rocolita <bernie@dorg.tech>
2024-03-25 15:25:34 -0400

    refactored nested loop in generate_roman_numerals

--- harmony_module.py
+++ harmony_module.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from music21 import *
-from mido import Message
 from midi_to_notation_translator import get_key_sigs
+from timeit import timeit
 
 razumovsky = corpus.parse('beethoven/opus59no1/movement1')
 # Define file to be searched
@@ -10,21 +10,38 @@ czerny = converter.parse('./test midi files/czerny_299_1.midi')
 
 
 # find the chords present in the midi file
+
+"""
+    generate_roman_numerals() function results:
+
+    CZERNY TEST:
+        Execution time: 0.50s
+        Staff count: 2
+        Measure count: 27
+
+    RAZUMOVSKY TEST:
+        Execution time: 11.22 seconds
+        Staff count: 4
+        Measure count: 400
+"""
+# Time complexity: O(N^2)
 def generate_roman_numerals(part):
     keys = get_key_sigs(part)
-    chords = part.chordify(removeRedundantPitches=True)
-    numeral_list = []
+    chords = part.chordify(removeRedundantPitches=True).flatten(retainContainers=True).getElementsByClass(chord.Chord)
+    numeral_list = [] # could be more efficient with a hash map
 
-    for measure in chords.getElementsByClass(stream.Measure):
-        measure_chords = []
-        
-        for c in measure.flatten().getElementsByClass(chord.Chord):
-            rn = roman.romanNumeralFromChord(c, keys)
-            measure_chords.append(rn.figure)
-    
-        numeral_list.append(measure_chords)
+    # iterate through the stream.Score to get the measures
+    for i in chords:
+        rn = roman.romanNumeralFromChord(i, keys)
+        numeral_list.append(rn.romanNumeral)
+
     return numeral_list
 
+# function to find the harmonic outline of the piece and point out new tonics
+def find_harmonic_outline(romans):
+    pass
 
-#help(key.Key)
-print(generate_roman_numerals(razumovsky))
+
+# use of timeit to measure runtime in seconds
+time_taken = timeit('generate_roman_numerals(czerny)', globals=globals(), number=1)
+print("Time taken:", time_taken)
